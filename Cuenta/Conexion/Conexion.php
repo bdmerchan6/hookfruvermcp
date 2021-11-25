c234a86ff658bc9cc9e07f9df27d64b3f7d4cfb5
vamendzo19 <vamedoza19@misena.edu.co>
2021-11-25 08:46:52 -0500

    arreglo de conexio

--- Cuenta/Conexion/Conexion.php
+++ Cuenta/Conexion/Conexion.php
@@ -1,16 +1,13 @@
 <?php  
 	$contrasena = 'FruverMCP123';
 	$usuario = 'u991668360_fruvermcp';
+	// $port= '3306';
 	$nombrebd= 'u991668360_fruvermcp';
-	$port= '3306';
+	
 
 	try {
-		$bd = new PDO(
-			'mysql:host=sql437.main-hosting.eu;
-			dbname='.$nombrebd,
-			$usuario,
-			$contrasena,
-			$port,
+		$bd = new PDO('mysql:host=sql437.main-hosting.eu;port=3306;dbname='.$nombrebd,$usuario,$contrasena,
+			
 			array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
 		);
 	} catch (Exception $e) {
